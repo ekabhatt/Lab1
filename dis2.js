93e590a304512aa148816d363887e5de38330727
Ekabhat Charnnarong <ekabhat@bu.edu>
2026-09-18 11:48:56 -0400

    fixed flex box

--- dis2.js
+++ dis2.js
@@ -2,13 +2,13 @@ let isScary=false;
 
 function scaryToggle(){
     if (isScary === false) {
-        document.getElementById("scaryimg").style.display="block";
+        document.getElementById("scaryimg").style.display="flex";
         document.getElementById("cuteimg").style.display="none";
         isScary=true;
     }
     else {
         document.getElementById("scaryimg").style.display="none";
-        document.getElementById("cuteimg").style.display="block";
+        document.getElementById("cuteimg").style.display="flex";
         isScary=false;
     }
 }
