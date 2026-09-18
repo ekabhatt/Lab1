24aec80ec34e2b6fe238f23da80e50f7b269abb6
Ekabhat Charnnarong <ekabhat@bu.edu>
2026-09-18 11:35:09 -0400

    added scary tab and button

---
+++ dis2.js
@@ -0,0 +1,14 @@
+let isScary=false;
+
+function scaryToggle(){
+    if (isScary === false) {
+        document.getElementById("scaryimg").style.display="block";
+        document.getElementById("cuteimg").style.display="none";
+        isScary=true;
+    }
+    else {
+        document.getElementById("scaryimg").style.display="none";
+        document.getElementById("cuteimg").style.display="block";
+        isScary=false;
+    }
+}
